bb0ab9506b1a13e801b1ea9c49ee07abe017a93a
anshuopinion <anshusopinion@gmail.com>
2022-09-28 13:27:46 +0530

    project4.2

--- index.js
+++ index.js
@@ -1,76 +1,109 @@
-const projectContainer = document.querySelector(".project_container");
+// Part 1
+// Create a Math question
+// Math question will have a random generated
+// Question Type Multiplicatin question  with random number range 1-10
+// Answer will be the product of the random number range and the random number range
+// User will have to answer question
+// On submit  answer answer will be compared with random generated answer
+// If answer will be correct than score will be incremented
+// If answer will be wrong than score will be decremented
 
-const projects = [
-  {
-    image: "images/p1.png",
-    title: "Life Timer App",
-    url: "../project1-fix/index.html",
-  },
-  {
-    image: "images/p2.png",
-    title: "Github User Search App",
-    url: "../project2/index.html",
-  },
-  {
-    image: "images/p3.png",
-    title: "Percentage Calculator App",
-    url: "../project3/index.html",
-  },
-  {
-    image: "images/p4.png",
-    title: "Math Quiz App",
-    url: "../project4.2/index.html",
-  },
-  {
-    image: "images/p5.png",
-    title: "Search & Filters Product App",
-    url: "../project5/index.html",
-  },
-  {
-    image: "images/p6.png",
-    title: "Color Generator App",
-    url: "../project6/index.html",
-  },
-  {
-    image: "images/p7.png",
-    title: "QR Code Generator App",
-    url: "../project7/index.html",
-  },
-  {
-    image: "images/p8.png",
-    title: "Expense Tracker App",
-    url: "../project8.3/index.html",
-  },
-  {
-    image: "images/p9.png",
-    title: "Password Generator App",
-    url: "../project9/index.html",
-  },
-  {
-    image: "images/p10.png",
-    title: "Drawer + Dropdown",
-    url: "../project10/index.html",
-  },
-];
+// Generate 4 Types of question
+// For Subtract first number should be greater than second number also for Divide
+// Store the score in local storage and display the score on the screen
+// Give Feedback to user using toast
 
-const showProjects = () => {
-  let output = "";
-  projects.forEach(
-    ({ image, title, url }, i) =>
-      (output += `
-      <div class="grid_item">
-      <div class="card">
-        <img src="${image}" alt="Project ${i}" />
-        <a href="${url}">
-          <div class="card_content">
-            <h3>${title}</h3>
-          </div>
-        </a>
-      </div>
-    </div>
-    `)
-  );
-  projectContainer.innerHTML = output;
+const questionEl = document.getElementById("question");
+const questionFormEl = document.getElementById("questionForm");
+const scoreEl = document.getElementById("score");
+let storedAnswer;
+let score = localStorage.getItem("score");
+const randomNumber = (min, max) => {
+  return Math.floor(Math.random() * (max - min + 1) + min);
 };
 
-showProjects();
+const generateQuestion = () => {
+  const randomNumber1 = randomNumber(1, 10);
+  const randomNumber2 = randomNumber(1, 10);
+  const questionType = randomNumber(1, 4);
+
+  let firstNumber;
+  let secondNumber;
+
+  if (randomNumber1 > randomNumber2 && questionType > 2) {
+    firstNumber = randomNumber1;
+    secondNumber = randomNumber2;
+  } else {
+    firstNumber = randomNumber2;
+    secondNumber = randomNumber1;
+  }
+
+  let question;
+  let answer;
+
+  switch (questionType) {
+    case 1:
+      question = `Q. What is ${firstNumber} multiply by ${secondNumber} ?`;
+      answer = firstNumber * secondNumber;
+      break;
+    case 2:
+      question = `Q. What is ${firstNumber} Add to ${secondNumber} ?`;
+      answer = firstNumber + secondNumber;
+      break;
+    case 3:
+      question = `Q. What is ${firstNumber} Divided By ${secondNumber} ?`;
+      answer = firstNumber / secondNumber;
+      break;
+    case 4:
+      question = `Q. What is ${firstNumber} Subtract from ${secondNumber} ?`;
+      answer = firstNumber - secondNumber;
+      break;
+    default:
+      question = `Q. What is ${firstNumber} Subtract from ${secondNumber} ?`;
+      answer = firstNumber - secondNumber;
+      break;
+  }
+
+  return { question, answer };
+};
+
+const showQuestion = () => {
+  const { question, answer } = generateQuestion();
+  questionEl.innerText = question;
+  scoreEl.innerText = score;
+  storedAnswer = answer;
+};
+showQuestion();
+
+const checkAnswer = (event) => {
+  event.preventDefault();
+  const formData = new FormData(questionFormEl);
+
+  const userAnswer = +formData.get("answer");
+  if (userAnswer === storedAnswer) {
+    score += 1;
+    Toastify({
+      text: `Your are wrong and your score is ${score}`,
+      gravity: "bottom",
+      position: "center",
+      style: {
+        background: "linear-gradient(to right, #00b09b, #96c93d)",
+      },
+    }).showToast();
+  } else {
+    score -= 1;
+    Toastify({
+      text: `Your are wrong and your score is ${score}`,
+      gravity: "bottom",
+      position: "center",
+      style: {
+        background: "linear-gradient(to right, #e33217, #ff001e)",
+      },
+    }).showToast();
+  }
+  scoreEl.innerText = score;
+  localStorage.setItem("score", score);
+  event.target.reset();
+  showQuestion();
+  console.log("answer", userAnswer);
+};
