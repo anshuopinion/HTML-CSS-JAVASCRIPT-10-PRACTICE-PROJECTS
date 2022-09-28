dcc3b85bf79519fe6510c5fb74804e23dbdd203a
anshuopinion <anshusopinion@gmail.com>
2022-09-28 13:28:26 +0530

    project5

--- index.js
+++ index.js
@@ -1,76 +1,62 @@
-const projectContainer = document.querySelector(".project_container");
+(async () => {
+  const productContainerEl = document.getElementById("productContainer");
+  const searchInputEl = document.getElementById("searchInput");
 
-const projects = [
-  {
-    image: "images/p1.png",
-    title: "Life Timer App",
-    url: "../project1-fix/index.html",
-  },
-  {
-    image: "images/p2.png",
-    title: "Github User Search App",
-    url: "../project2/index.html",
-  },
-  {
-    image: "images/p3.png",
-    title: "Percentage Calculator App",
-    url: "../project3/index.html",
-  },
-  {
-    image: "images/p4.png",
-    title: "Math Quiz App",
-    url: "../project4.2/index.html",
-  },
-  {
-    image: "images/p5.png",
-    title: "Search & Filters Product App",
-    url: "../project5/index.html",
-  },
-  {
-    image: "images/p6.png",
-    title: "Color Generator App",
-    url: "../project6/index.html",
-  },
-  {
-    image: "images/p7.png",
-    title: "QR Code Generator App",
-    url: "../project7/index.html",
-  },
-  {
-    image: "images/p8.png",
-    title: "Expense Tracker App",
-    url: "../project8.3/index.html",
-  },
-  {
-    image: "images/p9.png",
-    title: "Password Generator App",
-    url: "../project9/index.html",
-  },
-  {
-    image: "images/p10.png",
-    title: "Drawer + Dropdown",
-    url: "../project10/index.html",
-  },
-];
+  const url = "https://fakestoreapi.com/products";
+  const fetchProducts = async () => {
+    try {
+      const res = await fetch(url);
+      return await res.json();
+    } catch (error) {
+      return error;
+    }
+  };
+  const products = await fetchProducts();
 
-const showProjects = () => {
-  let output = "";
-  projects.forEach(
-    ({ image, title, url }, i) =>
-      (output += `
-      <div class="grid_item">
-      <div class="card">
-        <img src="${image}" alt="Project ${i}" />
-        <a href="${url}">
-          <div class="card_content">
-            <h3>${title}</h3>
-          </div>
-        </a>
-      </div>
-    </div>
-    `)
-  );
-  projectContainer.innerHTML = output;
-};
+  const generateProducts = (product) => {
+    return `<div class="product_card">	
+      <div class="image_container">	
+        <img	
+          src="${product.image}"	
+          alt=""	
+        />	
+      </div>	
+      <div class="product_content">	
+        <h2>	
+         ${product.title}	
+        </h2>	
+        <p>	
+         ${product.description.split(" ").slice(0, 20).join(" ")}	
+        </p>	
+        <button>${product.price} $</button>	
+      </div>	
+    </div>`;
+  };
 
-showProjects();
+  const renderProducts = (products) => {
+    productContainerEl.innerHTML = "";
+    products.forEach((product) => {
+      productContainerEl.innerHTML += generateProducts(product);
+    });
+  };
+
+  const checkTextContain = (text, searchText) => {
+    return text.toString().toLowerCase().includes(searchText);
+  };
+
+  const filterHandler = (event) => {
+    const searchText = event.target.value.toLowerCase();
+    const filteredProducts = products.filter((product) => {
+      return (
+        checkTextContain(product.description, searchText) ||
+        checkTextContain(product.title, searchText) ||
+        checkTextContain(product.price, searchText)
+      );
+    });
+    renderProducts(filteredProducts);
+  };
+
+  searchInputEl.addEventListener("keyup", filterHandler);
+
+  renderProducts(products);
+})();
