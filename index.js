70c206f435890bd29ab11941c7c6f60f30bceecf
anshuopinion <anshusopinion@gmail.com>
2022-09-28 13:34:10 +0530

    project 9 done

--- index.js
+++ index.js
@@ -1,76 +1,59 @@
-const projectContainer = document.querySelector(".project_container");
+let passwordLength = 8;
 
-const projects = [
-  {
-    image: "images/p1.png",
-    title: "Life Timer App",
-    url: "../project1-fix/index.html",
-  },
-  {
-    image: "images/p2.png",
-    title: "Github User Search App",
-    url: "../project2/index.html",
-  },
-  {
-    image: "images/p3.png",
-    title: "Percentage Calculator App",
-    url: "../project3/index.html",
-  },
-  {
-    image: "images/p4.png",
-    title: "Math Quiz App",
-    url: "../project4.2/index.html",
-  },
-  {
-    image: "images/p5.png",
-    title: "Search & Filters Product App",
-    url: "../project5/index.html",
-  },
-  {
-    image: "images/p6.png",
-    title: "Color Generator App",
-    url: "../project6/index.html",
-  },
-  {
-    image: "images/p7.png",
-    title: "QR Code Generator App",
-    url: "../project7/index.html",
-  },
-  {
-    image: "images/p8.png",
-    title: "Expense Tracker App",
-    url: "../project8.3/index.html",
-  },
-  {
-    image: "images/p9.png",
-    title: "Password Generator App",
-    url: "../project9/index.html",
-  },
-  {
-    image: "images/p10.png",
-    title: "Drawer + Dropdown",
-    url: "../project10/index.html",
-  },
-];
+let isUpperCase = false;
+let isNumbers = false;
+let isSymbols = false;
 
-const showProjects = () => {
-  let output = "";
-  projects.forEach(
-    ({ image, title, url }, i) =>
-      (output += `
-      <div class="grid_item">
-      <div class="card">
-        <img src="${image}" alt="Project ${i}" />
-        <a href="${url}">
-          <div class="card_content">
-            <h3>${title}</h3>
-          </div>
-        </a>
-      </div>
-    </div>
-    `)
-  );
-  projectContainer.innerHTML = output;
+const passwordRangeInputEl = document.getElementById("passRangeInput");
+const passRangeValueEl = document.getElementById("passRangeValue");
+const genBtn = document.getElementById("genBtn");
+const passwordEl = document.getElementById("password");
+
+const generatePassword = (passLength) => {
+  const lowerCaseLetters = "abcdefghijklmnopqrstuvwxyz";
+  const upperCaseLetters = isUpperCase ? "ABCDEFGHIJKLMNOPQRSTUVWXYZ" : "";
+  const numbers = isNumbers ? "0123456789" : "";
+  const symbols = isSymbols ? "!@#$%^&*()_+" : "";
+
+  const passwordChar = lowerCaseLetters + upperCaseLetters + numbers + symbols;
+
+  let password = "";
+
+  for (let i = 0; i < passLength; i++) {
+    const charIndex = Math.floor(Math.random() * passwordChar.length);
+    password += passwordChar[charIndex];
+  }
+  return password;
 };
 
-showProjects();
+passwordRangeInputEl.addEventListener("input", (e) => {
+  passwordLength = +e.target.value;
+  passRangeValueEl.innerText = passwordLength;
+});
+
+genBtn.addEventListener("click", () => {
+  const upperCaseCheckEl = document.getElementById("uppercase");
+  const numbersCheckEl = document.getElementById("numbers");
+  const symbolsCheckEl = document.getElementById("symbols");
+
+  isUpperCase = upperCaseCheckEl.checked;
+  isNumbers = numbersCheckEl.checked;
+  isSymbols = symbolsCheckEl.checked;
+
+  const password = generatePassword(passwordLength);
+  passwordEl.innerHTML = password;
+  console.log("password", password);
+});
+
+passwordEl.addEventListener("click", (e) => {
+  if (e.target.innerText.length > 0) {
+    navigator.clipboard
+      .writeText(passwordEl.innerText)
+      .then(() => {
+        alert("Copied to clipboard");
+      })
+      .catch((err) => {
+        alert("could not copy");
+      });
+  }
+});
